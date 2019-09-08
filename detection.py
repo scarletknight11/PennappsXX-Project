9a924f806356b6d6ec2e2431fd81b8ed5c6d1a74
Sanjit Singh <sanjitpsingh@yahoo.com>
2019-09-08 02:17:37 -0400

    Face Detection & Eye Recognition works, files are better organized, code ie more usable and clean

---
+++ detection.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+# Read the input image
+
+cap =cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ey,ey), (ex+ew, ey+eh), (0,255,0), 5)
+
+    # Display the output
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
